d4d8209e6404d53ad90c84219e2897fde5dc84cb
junai <lucifermjm@gmail.com>
2021-06-29 15:25:49 +0500

    Warnings removed

--- src/ProjectWrapper/Projects.js
+++ src/ProjectWrapper/Projects.js
@@ -10,7 +10,7 @@ const Projects = () => {
     <div className="project-container">
       {ProjectData.map((show, index) => {
         return <Project {...show} key={index} />;
-      })}
+      })}{" "}
     </div>
   );
 };
@@ -22,12 +22,12 @@ const Project = (props) => {
       <img src={img} alt="" />
       <div className="intro">
         <h2>
-          Project Name : <span>{name}</span>
+          Project Name: <span> {name} </span>
         </h2>
         <h4>
-          Topic Coverd : <span>{topic}</span>
+          Topic Coverd: <span> {topic} </span>
         </h4>
-        <a href={link} target="_blank">
+        <a href={link} target="_blank" rel="noopener noreferrer">
           Go to website
         </a>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import reactDom from "react-dom";
-import reactDOm from "react-dom";
 
 // css files
 
