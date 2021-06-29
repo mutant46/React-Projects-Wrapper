32d6c6fa5e0104b167c8c00579f5f0f380e1cfb8
junai <lucifermjm@gmail.com>
2021-06-29 14:50:24 +0500

    Basic Project Wrapper

---
+++ src/ProjectWrapper/Projects.js
@@ -0,0 +1,38 @@
+// import data
+import { ProjectData } from "../data";
+
+// importing css files
+
+import "./projects.css";
+
+const Projects = () => {
+  return (
+    <div className="project-container">
+      {ProjectData.map((show, index) => {
+        return <Project {...show} key={index} />;
+      })}
+    </div>
+  );
+};
+
+const Project = (props) => {
+  const { name, img, link, topic } = props;
+  return (
+    <div className="project">
+      <img src={img} alt="" />
+      <div className="intro">
+        <h2>
+          Project Name : <span>{name}</span>
+        </h2>
+        <h4>
+          Topic Coverd : <span>{topic}</span>
+        </h4>
+        <a href={link} target="_blank">
+          Go to website
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
---
+++ src/viewport/Viewport.js
@@ -0,0 +1,18 @@
+import "./viewport.css";
+
+import React from "react";
+
+const Viewport = () => {
+  return (
+    <React.Fragment>
+      <div className="main-container">
+        <h1>MuTanT46 React Projects</h1>
+        <span>
+          <img src="arrow.png" alt="" />
+        </span>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Viewport;
