91ed3bd987a152fd33c83af69f0056592cd7cf75
junai <lucifermjm@gmail.com>
2021-06-30 20:50:02 +0500

    project 1 added

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 export const ProjectData = [{
-    'img': 'https://images.unsplash.com/photo-1422207258071-70754198c4a2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=741&q=80',
+    'img': 'project1.png',
     'name': 'Birthday Remainder',
     'topic': 'UseState',
-    'link': 'http://mutant46.pythonanywhere.com'
+    'link': 'https://usestate-project.netlify.app/'
 }]
