f0041029c6c18138f01dd407828117b09b6be8a9
junai <lucifermjm@gmail.com>
2021-07-15 22:29:05 +0500

    Second project added

--- src/data.js
+++ src/data.js
@@ -1,6 +1,13 @@
 export const ProjectData = [{
-    'img': 'project-1.png',
-    'name': 'Birthday Remainder',
-    'topic': 'UseState',
-    'link': 'https://usestate-project.netlify.app/'
-}]
+        'img': 'project-1.png',
+        'name': 'Birthday Remainder',
+        'topic': 'UseState',
+        'link': 'https://usestate-project.netlify.app/'
+    },
+    {
+        'img': 'project-2.png',
+        'name': 'Tour List',
+        'topic': 'UseEffect & Conditional rendering',
+        'link': 'https://useeffect-project.netlify.app/'
+    }
+]
